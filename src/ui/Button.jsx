cc4712d0c81f654afd87999670a21b7c918ab37e
16gsanket <16gsanket@gmail.com>
2024-04-02 21:04:52 +0530

    reduxtoolkit used.... for user...next:... try reading user form home and setting it to store->user->state...in fresh branch v1

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -11,7 +11,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
     small: base + ' px-3 py-2 md:px-5 md:py-2.5 text-xs',
-    secondary:'px-4 py-2.5 sm:px-6 sm:py-3.5 inline-block rounded-full border-2 border-stone-300 bg-transparent font-semibold uppercase tracking-wide text-stone-500 text-sm transition-colors duration-300 hover:bg-stone-800 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed'
+    secondary:
+      'px-4 py-2.5 sm:px-6 sm:py-3.5 inline-block rounded-full border-2 border-stone-300 bg-transparent font-semibold uppercase tracking-wide text-stone-500 text-sm transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed',
   };
 
   if (to) {
